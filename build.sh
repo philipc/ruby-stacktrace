02937d14b8c8ddaa8a2a827626f63202ccb8649c
Julia Evans <julia@jvns.ca>
2016-07-09 20:34:21 +0200

    horrible build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-command=`cargo build --verbose 2>&1 | grep cc | grep note | grep -Eo cc.+ | sed 's/"//g' | grep stacktrace | sed 's/-pie//'`
+command=`cargo build --verbose 2>&1 | tee /tmp/out | grep cc | grep note | grep -Eo cc.+ | sed 's/"//g' | grep stacktrace | sed 's/-pie//'`
+cat /tmp/out
 set -x
 eval "$command -lelf"
